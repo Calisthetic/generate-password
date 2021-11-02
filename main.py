43c4e8c6d59b24d1326756a4d1f54ed2b527e9ed
KalashVovan <72902666+KalashVovan@users.noreply.github.com>
2021-11-02 12:15:44 +0500

    New version 0.0.4

--- main.py
+++ main.py
@@ -1,64 +1,91 @@
-import random
+from Password import Password
 import datetime
 import os
+from rich import print, box
+from rich.layout import Layout
+from rich.panel import Panel
+from rich.console import Console, Group
+from rich.text import Text
 
-# Набор доступных символов
-ARRAY_SYMBOLS = [
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '.', ',',
-    '=', '|', '/', '<', '>', ';', ':', '`', '~', '{', '}', '[', ']', '?', "'"
-]
-# Получаем количество символов в пароле
-input_count = input('Введите количество символов в пароле: ')
-if input_count.isdigit():
-    count_symbols = int(input_count)
+
+def file_name():
+    text_datetime = f'{datetime.datetime.now()}'
+    symbols_replace = ['-', ' ', ':', '.']
+    fn = ''
+    for s in text_datetime:
+        is_write = True
+        for sr in symbols_replace:
+            if s == sr:
+                fn += '_'
+                is_write = False
+        if is_write:
+            fn += s
+    return fn
+
+
+password = Password()
+
+count_symbols = input("Введите количество символов в пароле: ")
+
+if count_symbols.isdigit():
+    password.generation(int(count_symbols))
 else:
-    count_symbols = 8
-    print('Вы ввели строку, требуется число')
+    password.generation(None)
 
-# Считаем количество возможных комбинаций
-count_variant = len(ARRAY_SYMBOLS) ** count_symbols
+# print(f'Версия рограммы: v0.0.4')
+# print(f'Доступные символы: {password.get_array_symbols()}')
+# print(f'Количество доступных символов: {len(password.get_array_symbols())}')
+# print(f'Количество возможных комбинаций: {password.count_variant}')
+#
+# print(f'Сгенерированный пароль: {password}')
+
+layout = Layout(name="info")
+
+print_count_array_symbols = Text.form_markup(
+    f'Количество доступных символов: {len(password.get_array_symbols())}',
+    style="bold yellow"
+)
+
+print_array_symbols = Text.form_markup(
+    f'Доступные символы: {password.get_array_symbols()}',
+    style="bold yellow"
+)
+
+print_count_variant = Text.form_markup(
+    f'Количество возможных комбинаций: {password.count_variant}',
+    style="italic #af00ff"
+)
+
+print_password = Text.form_markup(
+    f'Сгенерированный пароль: {password.password}',
+    style="bold yellow"
+)
 
 
-# Получить случайный символ
-def random_symbols():
-    return ARRAY_SYMBOLS[random.randint(0, len(ARRAY_SYMBOLS) - 1)]
+console = Console()
+layout = Layout(name="info")
 
+layout.update(
+    Panel(
+        Group(
+            print_count_array_symbols,
+            print_array_symbols,
+            print_count_variant,
+            print_password,
+        ),
+        box=box.ROUNDED,
+        title="Информация",
+        subtitle="Версия рограммы: v0.0.4",
+    )
+)
 
-print(f'Версия рограммы: v0.0.2')
-print(f'Доступные символы: {ARRAY_SYMBOLS}')
-print(f'Количество доступных символов: {len(ARRAY_SYMBOLS)}')
-print(f'Количество возможных комбинаций: {count_variant}')
-
-# Генерируем пароль
-password = ''
-for i in range(0, count_symbols):
-    password = password + f'{random_symbols()}'
-
-print(f'Сгенерированный пароль: {password}')
-
-text_datetime = f'{datetime.datetime.now()}'
-symbols_replace = ['-', ' ', ':', '.']
-file_name = ''
-for s in text_datetime:
-    is_write = True
-    for sr in symbols_replace:
-        if s == sr:
-            file_name += '_'
-            is_write = False
-    if is_write:
-        file_name += s
+console.print(layout)
 
 if not os.path.exists('passwords'):
     os.mkdir('passwords')
 
-#
+# Запись пароля в файл
 with open(f'password/{file_name}_password.txt', 'a') as password_string:
     password_string.write('{}\n'.format(f'{password}'))
 
 input()
-
